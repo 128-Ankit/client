25024ebf02be30e0d24cf5e72a9d2dcc69837273
Ankit kumar <abhishekankit128@gmail.com>
2025-03-31 11:07:24 +0530

    frontend for portfolio builder

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://portfolio-builder-production-8a98.up.railway.app';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // User Signup
 export const signup = async (userData) => {
